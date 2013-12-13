4fa5b35026664159dc43f4e0e743ab9e0b230c0f
Adabriand <adabriand@gmail.com>
2013-12-13 15:19:16 -0300

    Tutorial

---
+++ Sun4All/setup.py
@@ -0,0 +1,22 @@
+import pbclient
+
+def contents(filename):
+    return file(filename).read()
+
+# settings
+pbclient.set('api_key', "c008cbd5-7885-4c49-a0fe-6cdee926651f")
+pbclient.set('endpoint', 'http://localhost/pybossa')
+
+# Create the app
+#response = pbclient.create_app('Sun for All', 'Sun4All','The aim of the project is the collection of over 30,000 images of the Sun (spectroheliograms) existing at the Astronomical Observatory of the University of Coimbra, the result of work of more than 80 years of daily observations of the Sun started in 1926.');
+
+#update app
+pyBossaApp = pbclient.find_app(short_name='Sun4All')[0];
+#pyBossaApp.long_description = '- add long description -';
+pyBossaApp.info['task_presenter'] = contents('../Site/template.html')
+pyBossaApp.info['tutorial'] = contents('../Site/tutorial.html')
+pyBossaApp.info['thumbnail'] = "http://societic.ibercivis.es/sun4all/icon.jpg"
+pyBossaApp.category_id = 2
+
+#pyBossaApp.info['tutorial'] = contents('tutorial.html')
+pbclient.update_app(pyBossaApp)

62ab469a5901eb7ef711b87af352ad8bf54950df
Adabriand <adabriand@gmail.com>
2014-03-10 17:57:53 -0300

    New tutorials for PT and EN languages

--- Site/static/js/sun4all.i18n.js
+++ Site/static/js/sun4all.i18n.js
@@ -16,7 +16,7 @@ var default_dict = {
 		"twitter-share" : "Click to Share on Twitter",
 		"google-plus-share" : "Click to Share on Google+",
 		"tutorial-title": "Sun4All Tutorial",
-		"tutorial-p1": "<strong>Hello!</strong><br> <br> First of all, thank you so much for giving us your time to help us with	Sun4All! This tutorial will maximize your experience and contribution, so please, read it carefully. <br> <br> The project is based on an asset of over 30000 images of the Sun (spectroheliograms) that are kept at the Astronomical Observatory of the University of Coimbra, as result of a work of over 80 years of daily solar observations, started in 1926. <br> <br> The analysis and counting of the sunspots is a very important aspect of the study of the phenomena occurring on the surface of Sun and to understand the solar activity and solar cycle. On theother hand studying the sunspots, besides being interesting itself, presents an important tool to understand much of what happens on the Earth climate. <br> <br> Thank you for your collaboration!",
+		"tutorial-p1": "<strong>Hello!</strong><br> <br> First of all, thank you so much for giving us your time to help us with	Sun4All! This tutorial will maximize your experience and contribution, so please, read it carefully. <br> <br> The project is based on an asset of over 30000 images of the Sun (spectroheliograms) that are kept at the Astronomical Observatory of the University of Coimbra, as result of a work of over 80 years of daily solar observations, started in 1926. <br> <br> The analysis and counting of the sunspots is a very important aspect of the study of the phenomena occurring on the surface of Sun and to understand the solar activity and solar cycle. On the other hand studying the sunspots, besides being interesting itself, presents an important tool to understand much of what happens on the Earth climate. <br> <br> Thank you for your collaboration!",
 		"tutorial-p2-1": "In this application you’ll find an image of the sun - a spectroheliogram - dated with the day when was taken. The one used in this example was taken in September, 03 – 1980 (see above the picture). <br> <br> The purpose of this application is to count sunspots and sunspots groups. To do that, we provide you with tools to <strong>manipulate the image</strong> and <strong>to do the counting</strong>. <br> <br> You can use icon",
 		"tutorial-p2-2": "to get more information about the	application and also the social network icons to spread the word!",
 		"tutorial-p3-1": "First of all, what can be considered as a <strong>sunspot</strong> and a <strong>sunspot group</strong>?",
@@ -35,15 +35,13 @@ var default_dict = {
 		"tutorial-p5-3": "Positive",
 		"tutorial-p5-4": "In an image in negative sunspots appear as white spots and in positive the sunspots appear as black spots.",
 		"tutorial-p6-1": "<strong>To mark the sunspots or sunspot groups</strong>, you have	located at the right side of the image the “Observation” box:",
-		"tutorial-p6-2": "The <strong>Observation box</strong> will help you to analyse the image by allowing you to “Add Sunspots”, “Add Sunspot Group”, and	“Add Information” that you may find relevant. <br> <br>	Additionally, if you wrongly marked a sunspot or sunspot group,	you can select it by clicking on the circle surrounding it, and then clicking on the button “Remove a Sunspot/Group”",
-		"tutorial-p7-1": "<strong>To mark sunspots</strong> just follow the next steps:",
-		"tutorial-p7-2": "1) First you have to create a square around the area you want to cover. To do this, click on the image using the left button of the mouse.",
-		"tutorial-p7-3": "2) Then, you can adjust the square to your spot easily by clicking in the square and dragging	it to the position you want.",
-		"tutorial-p7-4": "3) Finally, you have to go to the “Observation” box and click on “Add Sunspot” button which will mark your spot with a yellow circle!",
-		"tutorial-p8-1": "<strong>To mark sunspot groups</strong> just follow the next steps:",
-		"tutorial-p8-2": "1) First you have to create a	square around the area you want to cover. To do this, click on the image using the left button of the mouse.",
-		"tutorial-p8-3": "2) As before, you can adjust the square to your group easily by clicking in the square and dragging it to the position you want.",
-		"tutorial-p8-4": "3) Finally, you have to go to	the “Observation” box and click on “Add Sunspot Group” button which will mark your group with a blue circle!",
+		"tutorial-p6-2": "The <strong>Observation box</strong> will help you to analyse the image by allowing you to “Add Sunspots”, “Add Sunspot Group”, and	“Add Information” that you may find relevant. <br> <br>	Additionally, if you wrongly marked a sunspot or sunspot group, you can remove it by clicking on the button “Remove a Sunspot / Group” and then clicking on the mark(s) you want to remove.",
+		"tutorial-p7-1": "<strong>To mark sunspots</strong>, start by clicking on the button “Add Sunspot” in the Observation box.",
+		"tutorial-p7-2": "Then just click on the sunspots you observe in the image. Each sunspot is marked with a yellow cross.",
+		"tutorial-p7-3": "You can move the mark on the sunspot. Just click on it, maintain the left button of the mouse pressed and drag the mark to the right position.",
+		"tutorial-p8-1": "<strong>To mark sunspot groups</strong>, start by clicking on the button “Add Sunspot Group” in the Observation box.",
+		"tutorial-p8-2": "Then just delimit the groups of sunspots with a blue square.",
+		"tutorial-p8-3": "To better adjust the position of the square, just click on it, maintain the left button of the mouse pressed and drag the square to the right position.",
 		"tutorial-p9-1": "The summary of your analysis appears in “Your Data” box:",
 		"tutorial-p9-2": "The <strong>“Your Data” box</strong> summarises your analysis by telling you how many sunspots and sunspot groups you have counted so far. <br> <br>It also allows you to “Finish the task” or to “Start over” the analysis.",
 		"tutorial-p10-1": "Once you have counted every sunspots and sunspot groups in the spectroheliogram, you just have to click on the “Finish Task” button! <br> <br> Now you are ready to start contributing! You can also check on the ",
@@ -86,8 +84,8 @@ var pt_dict= {
 		"twitter-share" : "Clique para compartilhar no Twitter",
 		"google-plus-share" : "Clique para compartilhar no Google+",
 		"tutorial-title": "Tutorial do Sun4All",
-		"tutorial-p1": "<strong>Olá!</strong><br> <br> Começamos por agradecer a sua disponibilidade para colaborar com o projecto Sun4All! Este tutorial ajudá-lo-á a maximizar a sua aprendizagem e contribuição, por isso, pedimos que o leia com atenção. <br> <br> Este projecto baseia-se no espólio de 30,000 imagens do Sol (espectroheliogramas) mantidas pelo Observatório Astronómico da Universidade de Coimbra, resultado do trabalho de observações solares diárias ao longo de mais de 80 anos, iniciadas em 1926. A análise e contagem das manchas solares é um aspecto muito importante no estudo dos fenómenos que ocorrem na superfície do Sol, para a compreensão da actividade e ciclo solares. Por outro lado, é também uma importante ferramenta para compreender muito do que acontece com o Clima da Terra. <br> <br> Obrigado pela sua colaboração!",
-		"tutorial-p2-1": "Nesta aplicação, encontra uma imagem do Sol – um espectroheliograma – com a data em que a imagem foi obtida. A imagem no exemplo foi tirada a 6 de Setembro de 1980. (Texto no topo da imagem). <br> <br> O objectivo é contar as manchas e grupos de manchas solares. Para realizar esta tarefa, tem ao seu dispor várias ferramentas para <strong>manipular a imagem</strong> and <strong>fazer a contagem</strong>. <br> <br> Pode usar o ícone",
+		"tutorial-p1": "<strong>Olá!</strong><br> <br> Começamos por agradecer a sua disponibilidade para colaborar com o projecto Sun4All! Este tutorial ajudá-lo-á a maximizar a sua aprendizagem e contribuição, por isso, pedimos que o leia com atenção. <br> <br> Este projecto baseia-se no espólio de 30,000 imagens do Sol (espectroheliogramas) mantidas pelo Observatório Astronómico da Universidade de Coimbra, resultado do trabalho de observações solares diárias ao longo de mais de 80 anos, iniciadas em 1926. <br> <br> A análise e contagem das manchas solares é um aspecto muito importante no estudo dos fenómenos que ocorrem na superfície do Sol, para a compreensão da actividade e ciclo solares. Por outro lado, é também uma importante ferramenta para compreender muito do que acontece com o Clima da Terra. <br> <br> Obrigado pela sua colaboração!",
+		"tutorial-p2-1": "Nesta aplicação, encontra uma imagem do Sol – um espectroheliograma – com a data em que a imagem foi obtida. A imagem no exemplo foi tirada a 22 de Setembro de 1980. (Texto no topo da imagem). <br> <br> O objectivo é contar as manchas e grupos de manchas solares. Para realizar esta tarefa, tem ao seu dispor várias ferramentas para <strong>manipular a imagem</strong> and <strong>fazer a contagem</strong>. <br> <br> Pode usar o ícone",
 		"tutorial-p2-2": "para obter mais informação acerca da aplicação e também os ícones das redes sociais para passar a palavra!",
 		"tutorial-p3-1": "O que são <strong>manchas</strong> e <strong>grupos de manchas solares</strong>?",
 		"tutorial-p3-2": "Uma mancha solar é todo o ponto que seja detectado na superfície do Sol. Alguns exemplos:",
@@ -99,21 +97,19 @@ var pt_dict= {
 		"tutorial-p4-5": "Pode também navegar na imagem ampliada utilizando as setas: <br> Para baixo",
 		"tutorial-p4-6": "para cima ",
 		"tutorial-p4-7": "para a esquerda ",
-		"tutorial-p4-8": "or para a direita ",
+		"tutorial-p4-8": "ou para a direita ",
 		"tutorial-p5-1": "É também possível inverter a imagem entre o canal negativo e positivo para detectar as manchas solares mais facilmente.",
 		"tutorial-p5-2": "Negativo",
 		"tutorial-p5-3": "Positivo",
 		"tutorial-p5-4": "Numa imagem em negativo as manchas solares aparecem a branco, e numa imagem em positivo as manchas aparecem a preto.",
 		"tutorial-p6-1": "<strong>Para marcar manchas e grupos de manchas</strong>, deve utilizar os botões da caixa de Observação:",
-		"tutorial-p6-2": "A <strong>caixa de Observação</strong> ajuda-o a analisar a imagem permitindo “Marcar mancha”, “Marcar grupo de manchas”, e adicionar “Observações” que considere relevantes. <br> <br> Caso se engane a marcar uma mancha ou um grupo de manchas, pode remover a marcação seleccionando o círculo em causa e clicando no botão “Remover Mancha ou Grupo”.",
-		"tutorial-p7-1": "<strong>Para marcar uma mancha solar</strong>, siga os seguintes passos:",
-		"tutorial-p7-2": "1) Com o rato, comece por desenhar um quadrado que inclua a mancha solar, utilizando o botão esquerdo do rato.",
-		"tutorial-p7-3": "2) Pode ajudar a posição do quadrado, clicando no quadrado e arrastando-o para a posição desejada.",
-		"tutorial-p7-4": "3) Para marcar a mancha seleccionada, clique no botão “Marcar mancha” localizado na caixa de Observação. A mancha fica marcada com um círculo amarelo!",
-		"tutorial-p8-1": "<strong>Para marcar grupos de manchas</strong>, o procedimento é semelhante:",
-		"tutorial-p8-2": "1) Com o rato, comece por desenhar um quadrado que abranja o grupo de manchas solares, utilizando o botão esquerdo do rato.",
-		"tutorial-p8-3": "2) Pode ajustar a posição do quadrado, clicando no quadrado e arrastando-o para a posição desejada.",
-		"tutorial-p8-4": "3) Para marcar o grupo de manchas solares seleccionado, clique no botão “Marcar grupo de manchas” localizado na caixa de Observação. O grupo de manchas fica marcado com um círculo azul!",
+		"tutorial-p6-2": "A <strong>caixa de Observação</strong> ajuda-o a analisar a imagem permitindo “Marcar mancha”, “Marcar grupo de manchas”, e adicionar “Observações” que considere relevantes. <br> <br> Caso pretenda corrigir a marcação de uma mancha ou grupo de manchas, pode remover a marcação clicando no botão “Remover Mancha/Grupo” e em seguida na marca que pretende eliminar.",
+		"tutorial-p7-1": "<strong>Para marcar uma mancha solar</strong>, comece por clicar no botão “Marcar mancha” que se encontra na caixa de Observação.",
+		"tutorial-p7-2": "Em seguida só tem que clicar sobre as manchas que observa na imagem. Cada mancha é assinalada com uma cruz amarela.",
+		"tutorial-p7-3": "Pode ajustar a posição das suas marcações. Para isso basta clicar sobre a marca, manter o botão do lado esquerdo premido e arrastar a marca para a posição desejada.",
+		"tutorial-p8-1": "<strong>Para marcar grupos de manchas</strong>, comece por clicar no botão “Marcar Grupo” que se encontra na caixa de Observação.",
+		"tutorial-p8-2": "Em seguida só tem que delimitar os grupos de manchas desenhando um quadro azul.",
+		"tutorial-p8-3": "Pode ajustar a posição das suas marcações. Para isso basta clicar sobre a marca, manter o botão do lado esquerdo premido e arrastar a marca para a posição desejada.",
 		"tutorial-p9-1": "O sumário da sua análise aparece na caixa “Dados”:",
 		"tutorial-p9-2": "A caixa <strong>“Dados”</strong> sumaria os resultados da sua análise apresentando o número de manchas e grupos de manchas solares contadas. <br> <br>Quando considerar que a sua análise está terminada, clique no botão “Tarefa concluída”. Pode também optar por “Recomeçar” a análise.",
 		"tutorial-p10-1": "Pode agora começar a contribuir! Para obter mais informações pode também consultar a",
@@ -145,6 +141,8 @@ case 'pt-BR': var dict = pt_dict; break;
 default: var dict = default_dict;
 }
 
+//var dict = default_dict;
+
 var monthNames_en = ["January", "February", "March", "April", "May", "June",
                   "July", "August", "September", "October", "November", "December"];
 
@@ -152,7 +150,7 @@ var monthNames_pt = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
                      "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 function getDataTakenInfo(date) {
-	if (language == 'pt-BR') {
+	if (language == 'pt-BR' || language == 'pt') {
 		return 'Esta imagem foi obtida no dia ' + date.getDate() + ' de ' + monthNames_pt[date.getMonth()] + ' de ' + date.getFullYear();		
 	} else {
 		return 'This is an image taken in ' + monthNames_en[date.getMonth()] + ', ' + date.getDate() + ' - ' + date.getFullYear();
